0de37ec17d643693b105f8624efd1f3e3bc1975a
Nicolas <Nicolas@PC-NICOLAS>
2022-11-03 16:25:09 +0100

    move the content of the old "Application" layer to the "Presentation" layer

--- TournamentDistributionHexa.Application/Program.cs
+++ TournamentDistributionHexa.Application/Program.cs
@@ -1,35 +1,25 @@
-using Microsoft.EntityFrameworkCore;
-using TournamentDistributionHexa.Domain.Repositories;
-using TournamentDistributionHexa.Domain.Tournaments;
-using TournamentDistributionHexa.Infrastructure.Models;
-using TournamentDistributionHexa.Infrastructure.Repositories;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddScoped<ITournamentDomain, TournamentDomain>();
-builder.Services.AddScoped<ITournamentRepository, TournamentRepositoryAdapter>();
-builder.Services.AddDbContext<RepartitionTournoiContext>(
-    options => options.UseSqlServer("name=ConnectionStrings:RepartitionTournoiContext"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
